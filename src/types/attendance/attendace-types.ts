d17e6183fc87b325afb1bcc60d44cf46cce687d5
HibaMariyam <hibunalakath@gmail.com>
2024-12-19 14:58:23 +0530

    done

--- src/types/attendance/attendace-types.ts
+++ src/types/attendance/attendace-types.ts
@@ -1,3 +1,12 @@
-export interface getWorkUpdatesResponse{
-    message: string,
-}
+
+export interface WorkUpdate {
+    id: number
+    user_id: number
+    image?: string
+    remarks: string
+    created_at: string
+    updated_at: string
+    deleted_at: string
+    image_url?: string
+  }
+  
